b2e12ad740f7fe653909b82e04953af2217c6f5b
adrian <adrian1230@wp.pl>
2018-12-07 00:19:45 +0100

    first commit

--- src/main/java/HomeLoaderImpl.java
+++ src/main/java/HomeLoaderImpl.java
@@ -2,9 +2,9 @@ import factories.HomeFactory;
 import factories.HomeFactoryImpl;
 import models.Home;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeLoaderImpl implements HomeLoader {
     HomeFactory homeFactory = new HomeFactoryImpl();
@@ -12,7 +12,32 @@ public class HomeLoaderImpl implements HomeLoader {
     @Override
     public Home loadHome(String fileName, String separator) {
         //load file and use home factory to create home
+        HomeFactory homeFactoryLoad = new HomeFactoryImpl();
+        List<String> lista = new ArrayList<>();
 
-        return null;
+        BufferedReader bf = null;
+
+        try{
+            String strCurrentLine;
+            bf = new BufferedReader(new FileReader(fileName));
+
+            while ((strCurrentLine = bf.readLine()) != null) {
+//                System.out.println(strCurrentLine);
+                lista.add(strCurrentLine);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bf.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return homeFactoryLoad.createHome(lista, "%");
     }
 }
